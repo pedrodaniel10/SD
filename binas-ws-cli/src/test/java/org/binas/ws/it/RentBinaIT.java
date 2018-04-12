96063906d03ec07bc4a159b8274e868596730718
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2018-04-12 20:02:51 +0100

    some testes on rentBina

--- binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaIT.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.binas.ws.EmailExists_Exception;
 import org.binas.ws.InvalidEmail_Exception;
+import org.binas.ws.NoBinaAvail_Exception;
+import org.binas.ws.UserNotExists_Exception;
 import org.binas.ws.UserView;
 import org.junit.After;
 import org.junit.Test;
@@ -24,6 +26,44 @@ public class RentBinaIT extends BaseIT {
 	public void success() {
 		User user = new User(EMAIL);
 		client.rentBina(STATION_ID, EMAIL);
+	}
+	
+	@Test
+	public void userNotExists(){
+		try{
+			client.rentBina("A47_Station1", "teste@binas");	
+		}
+		catch(UserNotExists_Exception e){
+//			It should go here
+		}
+		catch(Exception e){
+//			any other exception the test should fail
+			fail();
+		}
+	}
+	
+	@Test 
+	public void noBinaAvailStation1(){
+		try {
+//			create user
+			client.activateUser("teste@binas");
+//			define station with 0 capacity
+			client.testInitStation("A47_Station1", 22, 7, 0, 2);
+			
+			client.rentBina("A47_Station1", "teste@binas");
+		} 
+		catch(NoBinaAvail_Exception e){
+			
+		}
+		catch (Exception e) {
+			fail();
+		}
+		
+	}
+	
+	@After
+	public void tearDown(){
+		client.testClear();
 	}
 }
 
