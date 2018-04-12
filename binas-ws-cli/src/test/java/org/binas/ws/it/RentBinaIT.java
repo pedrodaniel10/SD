7058f004839005d8eece726e2b7b5b937fbd7c15
Pedro Caldeira <caldeira.a.pedro@gmail.com>
2018-04-12 19:37:02 +0100

    inicio RentBinaIT

--- binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaIT.java
@@ -8,11 +8,22 @@ import org.binas.ws.UserView;
 import org.junit.After;
 import org.junit.Test;
 
+
+
+/**
+ * Test suite for rentBina, it is assumed that 3 station-ws and a binas-ws were initiated sucessefully.
+ */
+
 public class RentBinaIT extends BaseIT {
+	
+	private final int NUMBER_STATIONS = 3;
+	private final String STATION_ID = "A47_Binas";
+	private final String EMAIL = "caldeira.a.pedro@gmail.com";
 	
 	@Test
 	public void success() {
-		
+		User user = new User(EMAIL);
+		client.rentBina(STATION_ID, EMAIL);
 	}
 }
 
